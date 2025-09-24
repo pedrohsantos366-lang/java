745b827b6aca3277c4cd23c7d6f304e1e7b37812
Pedro Henrique Pais Santos <pet0160430@aluno.firjansenaisesi.com.br>
2025-09-23 21:24:25 -0300

    java2

---
+++ ConversorDeMoeda.java
@@ -0,0 +1,7 @@
+public class ConversorDeMoeda {
+    public static void main(String[] args){
+        double ValorEmDolar = 50;
+        double TaxaDeCambio = 5.25;
+        System.out.println("o Valor em reais(R$):"+ValorEmDolar*TaxaDeCambio);
+    }
+}
---
+++ NumMaior.java
@@ -0,0 +1,14 @@
+public class NumMaior {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 100;
+        if(num1 > num2){
+            System.out.println("o num "+num1+" e maior que o "+num2);
+        }else if(num2 > num1){
+            System.out.println("o num "+num2+ " e maior que o "+num1);
+        }else{
+            System.out.println("ambos os numeros sao iguais");
+        }
+    } 
+}
+1
